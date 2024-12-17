ee9f8cf6bf443c0bcdbe7fb26b7718bc4976595b
Sosy <sofiehvidt@live.dk>
2024-12-17 10:47:08 +0100

    kjh

---
+++ script.js
@@ -0,0 +1,12 @@
+var character = 
+document.getElementById("character");
+var block = 
+document.getElementById("block");
+
+function jump() {
+    character.classList.add("animate");
+
+setTimeout(funtion(){
+character.classList.remove("animate");
+    },500);
+}
